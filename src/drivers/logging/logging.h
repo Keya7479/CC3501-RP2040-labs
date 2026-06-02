e0ccb1249687366b6f0f1ebefdef46363f1a7eb4
jd119630 <keya.wong@my.jcu.edu.au>
2026-06-02 23:43:04 +1000

    Updated logging.h to have more specific documentation for functions.

--- src/drivers/logging/logging.h
+++ src/drivers/logging/logging.h
@@ -1,14 +1,22 @@
-#pragma once 
+#pragma once
 
-/// Represents the priority of a log message.
-enum LogLevel {
+/// Represents the priority of a log message. INFORMATION = 0, WARNING = 1, ERROR = 2
+enum LogLevel
+{
     INFORMATION,
     WARNING,
     ERROR,
 };
 
-/// Set the log level. Messages with a level below this threshold will be discarded.
+/**
+ * @brief Set the minimum log level to be printed. Correct syntax setLogLevel(LogLevel::level)
+ * @param newLevel Messages with a level below this threshold will be discarded
+ */
 void setLogLevel(LogLevel newLevel);
 
-/// Log a new message.
+/**
+ * @brief Log a new message with output as [time level]: msg. Correct syntax log(LogLevel::level, msg)
+ * @param level The priority and type of log
+ * @param msg Message to be printed
+ */
 void log(LogLevel level, const char *msg);
